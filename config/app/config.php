3a271d085efc73599fcd34af5520ae011ed5b0ba
Dean Blackborough <dean@g3d-development.com>
2022-07-24 12:35:00 +0100

    Release
    - Set the version and update the changelog

--- config/app/config.php
+++ config/app/config.php
@@ -10,6 +10,6 @@ return [
     'item_subtype_id' => env('ITEM_SUBTYPE_ID'),
     'cookie_user' => env('SESSION_NAME_USER'),
     'cookie_bearer' => env('SESSION_NAME_BEARER'),
-    'version' => '0.7.0',
-    'release_date' => '23rd July 2022'
+    'version' => '0.8.0',
+    'release_date' => '24th July 2022'
 ];
