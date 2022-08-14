370a8836b00821dcdaafa319b73477b7e72f8049
Dean Blackborough <dean@g3d-development.com>
2022-08-14 13:31:00 +0100

    Scratch Yahtzee disables bonus
    - Scratching Yahtzee should disable the bonus

--- public/js/score-sheet.js
+++ public/js/score-sheet.js
@@ -1,4 +1,4 @@
-import { display_selected_toast, disable_yahtzee_bonus_if_game_over } from './functions.js';
+import {display_selected_toast, disable_yahtzee_bonus_if_game_over, disable_yahtzee_bonus} from './functions.js';
 
 (function (axios) {
     'use strict'
@@ -287,6 +287,10 @@ import { display_selected_toast, disable_yahtzee_bonus_if_game_over } from './fu
                 player_final_score.innerText = response.data.score.upper + response.data.score.bonus + response.data.score.lower;
 
                 disable_yahtzee_bonus_if_game_over(response.data.turns);
+
+                if (payload.combo === 'yahtzee') {
+                    disable_yahtzee_bonus();
+                }
 
                 display_selected_toast(show_toast);
 
