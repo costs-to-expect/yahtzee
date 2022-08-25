a25ed2c1b3f61c198c100939601a60762e1776f1
Dean Blackborough <dean@g3d-development.com>
2022-08-25 14:09:06 +0100

    Player bonus message fix
    - Corrected a bonus message if you score 63 with fewer than 5 dice, should be >= 63

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -191,7 +191,7 @@ class Controller extends BaseController
             }
         }
 
-        if ($total > 63 && (count($dice_scored) + count($dice_scratched)) < 6) {
+        if ($total >= 63 && count($dice_scored) < 6) {
             $message = 'OK, OK, you have the bonus without even finishing!';
             return $this->playerBonusView($game_id, $player_id, $message);
         }
@@ -226,7 +226,7 @@ class Controller extends BaseController
             return $this->playerBonusView($game_id, $player_id, $message);
         }
 
-        if ($total > 63 && count($dice_scratched) > 0) {
+        if ($total >= 63 && count($dice_scratched) > 0) {
             $message = 'WOW, nothing like scoring the bonus whilst also scratching!';
             return $this->playerBonusView($game_id, $player_id, $message);
         }
