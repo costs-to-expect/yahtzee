8d02d5a5a714cea3cbbfe1a9302cfc1a40c5987c
Dean Blackborough <dean@g3d-development.com>
2023-10-12 22:51:09 +0100

    Registered via
    - Added registered via
    - Updated the change log and set the version

--- app/Api/Service.php
+++ app/Api/Service.php
@@ -367,7 +367,8 @@ class Service
             $uri['uri'],
             [
                 'name' => $payload['name'],
-                'email' => $payload['email']
+                'email' => $payload['email'],
+                'registered_via' => 'yahtzee',
             ]
         );
     }
